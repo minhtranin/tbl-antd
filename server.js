8b6fab9064b7bd56a96bbb5e754ac9dfa6be34f5
tcm <minhtran.in@outlook.com>
2020-02-13 10:20:49 +0700

    add api

--- server.js
+++ server.js
@@ -1,44 +1,35 @@
-var express = require('express')
-var cors = require('cors')
-var app = express()
-
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const fs = require('fs')
 app.use(cors())
 
 
 app.listen(8303, function () {
   console.log('CORS-enabled web server listening on port 8303')
 })
+
+// get
 app.get('/',cors(),(req,res)=>{
-    const data = [{
-        name: "minh",
-        old: "13",
-        phone: "23123123",
-        address: "1232131adsda"
-    },
-    {
-        name: "minh2",
-        old: "13",
-        phone: "23123123",
-        address: "1232131adsda"
-    },
-    {
-        name: "minh2.5",
-        old: "13",
-        phone: "23123123",
-        address: "1232131adsda"
-    },
-    {
-        name: "minh3",
-        old: "13",
-        phone: "23123123",
-        address: "1232131adsda"
-    },
-    {
-        name: "minh4",
-        old: "13",
-        phone: "23123123",
-        address: "1232131adsda"
-    }
-    ]
-    return res.send(data);
+    const data = fs.readFileSync('./db.json','utf8');
+    const response = JSON.parse(data);
+    return res.send(response);
+})
+// create
+app.get('/create',cors(), (req,res)=>{
+    // const name = req.query.name;
+    // const old = req.query.old;
+    // const phone = req.query.phone;
+    // const address =req.query.address;
+    // const db = {name,old,phone,address};
+    // console.log(db);
+    // const data = JSON.stringify(db);
+    // fs.writeFileSync('./db.json',data);
+    // return res.send(db);
+    const data = fs.readFileSync('./db.json','utf8');
+    const response = JSON.parse(data);
+    response.push(req.query);
+    const trans = JSON.stringify(response);
+    fs.writeFileSync('./db.json',trans);
+    return res.send(req.query)
 })
--- src/services/benefits.js
+++ src/services/benefits.js
@@ -3,3 +3,6 @@ import request from '../utils/request';
 export function query() {
   return request('http://localhost:8303/');
 }
+export function create(param) {
+  return request(`http://localhost:8303/create?name=${param.name}&&old=${param.old}&&phone=${param.phone}&&address=${param.address}`);
+}
